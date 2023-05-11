e0758a795b00fe736fa7d02c2b9820c1d9114e24
shohelelabs <76155636+shohelelabs@users.noreply.github.com>
2023-05-11 12:50:24 +0200

    Update email2sms.js

--- api/email2sms.js
+++ api/email2sms.js
@@ -20,11 +20,11 @@ module.exports = async (req, res) => {
     console.log({toNumberobj});
     console.log({toNumbers});
      
-     
+    const numberAddresses = toNumber.map( (item) => item.address);
+    console.log(numberAddresses);
    //Start mobile number Iteration
-    toNumbers.forEach((toNumber) => {  
-    const result = toNumber.map( (item) => item.address);
-    console.log(result);
+    numberAddresses.forEach((toNumber) => {  
+   
     const toAddress = addrs.parseOneAddress(result);
     const toName = toAddress.local;
     const fromAddress = addrs.parseOneAddress(from);
